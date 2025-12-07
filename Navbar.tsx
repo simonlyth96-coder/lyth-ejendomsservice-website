457eb2a1e142c1e738a83ce8f0b587aed11edc49
simonlyth96-coder <simonlyth96@gmail.com>
2025-12-07 01:48:03 +0100

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NAV_LINKS } from '../constants';
+import { NAV_LINKS } from './constants';
 import { Home, Menu, X } from 'lucide-react';
 
 const Navbar: React.FC = () => {
